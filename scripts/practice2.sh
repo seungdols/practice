aa9f2b0eb02479b054f019f7895f1cfa075d0b20
seungdols <seungdols0822@gmail.com>
2017-12-09 21:12:00 +0900

    update: 가져온 스크립트 조작해보기

--- scripts/practice2.sh
+++ scripts/practice2.sh
@@ -16,30 +16,36 @@ tell application "iTerm"
         split horizontally with default profile
     end tell
     tell session 1 of current tab of current window
-        split vertically with default profile
+        set test1 to (split vertically with default profile) 
+        tell test1 
+            write text "echo test1"
+        end tell 
+        write text "echo session 1"
     end tell
     tell session 3 of current tab of current window
         split vertically with default profile
+        write text "echo session 3"
     end tell
     tell session 5 of current tab of current window
         split vertically with default profile
+        write text "echo session 5"
     end tell
     tell session 7 of current tab of current window
         split vertically with default profile
+        write text "echo session 7"
     end tell
     tell session 9 of current tab of current window
         split vertically with default profile
         split vertically with default profile
+        write text "echo session 9"
     end tell
 
     # Establish connections & execute actions.
-    repeat with i from 1 to count of commands
-        tell session i of current tab of current window
-            write text (connection of item i of commands)
-            write text (action of item i of commands)
-        end tell
-    end repeat
+    #repeat with i from 1 to count of commands
+    #    tell session i of current tab of current window
+    #        write text (connection of item i of commands)
+    #        write text (action of item i of commands)
+    #    end tell
+    #end repeat
 end tell
 EOT
-    ;;
-esac
