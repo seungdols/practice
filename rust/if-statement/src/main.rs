18b475fe9c2a443a68c47792e47d3e64b2d6c26c
seungdols <seungdols0822@gmail.com>
2021-07-23 19:45:25 +0900

    Learn if-statement

---
+++ rust/if-statement/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+  let n = 45;
+  if n < 30 {
+    println!("The number is less than 30!");
+  } 
+  else if n == 45 {
+    println!("Number is 45!");
+  }
+  else {
+    println!("It was not 45!");
+  }
+}
