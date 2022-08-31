e90c069542f26a755ccea018c99389ec3824abfc
seungdols <seungdols0822@gmail.com>
2022-08-31 18:53:36 +0900

    Learn rawHTML

--- vuejs/hello-vue/src/components/DataBinding.vue
+++ vuejs/hello-vue/src/components/DataBinding.vue
@@ -1,12 +1,14 @@
 <template>
     <h1>Hello, {{ title }}</h1>
+    <div v-html="htmlString"></div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            title: "DataBinding"
+            title: "DataBinding",
+            htmlString: '<p style="color:red">This is a red string</p>'
         }
     }
 }
