9b072b2e73ed49bc7a57cd4f488f8247e6822a92
seungdols <seungdols0822@gmail.com>
2022-08-31 14:50:21 +0900

    Learn for loop

--- vuejs/hello-vue/src/components/HelloWorld.vue
+++ vuejs/hello-vue/src/components/HelloWorld.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="hello">
     <h1>{{ title }}</h1>
+
+    <ol>
+      <li v-for="todo in todos" v-bind:key="todo.text">
+        {{ todo.text }}
+      </li>
+    </ol>
   </div>
 </template>
 
@@ -9,9 +15,17 @@ export default {
 
   data() {
     return {
-      title: 'Hello World'
+      title: 'Hello World',
+      todos: [
+        { text: 'Learn VueJS' },
+        { text: 'Learn Kotlin' },
+        { text: 'Learn Go' },
+        { text: 'Learn Kafka' },
+        { text: 'Learn Rust' },
+        { text: 'Learn Python' },
+      ]
     }
   }
-  
+
 }
 </script>
