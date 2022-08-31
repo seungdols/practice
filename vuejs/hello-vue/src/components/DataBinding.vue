dc771289532f0faa4af6f96fc7bffdb6945f339f
seungdols <seungdols0822@gmail.com>
2022-08-31 16:28:00 +0900

    Learn Router

---
+++ vuejs/hello-vue/src/components/DataBinding.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Hello, {{ title }}</h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: "DataBinding"
+        }
+    }
+}
+</script>
